1aaa3aed37d12f4516c6ed34c396c8db10a95d08
katguerin <katguerin55@gmail.com>
2020-04-29 13:46:42 -0700

    adding these changes to the watchlist branch.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,17 @@
+//initialize foundation elements
 $(document).foundation();
 
+//declare global variable
 var contentEl = document.querySelector("#content");
 var watchlistEl = document.querySelector("#watchlist");
 var pageContentEl = document.querySelector("#pageContent");
 var searchButton = document.querySelector("#search-btn");
 var watchlistViewBtn = document.querySelector("#watchlist-view");
 var searchViewBtn = document.querySelector("#search-view");
+var containerEl = document.querySelector("#content-container");
 
 var currentQuery = '';
-var contentEl = document.querySelector("#content");
-var watchlistEl = document.querySelector("#watchlist");
-
+var savedToWatchlist = [];
 
 var createQuery = function() {
   //clear out contents of previous query
@@ -76,6 +77,7 @@ var createQuery = function() {
     var genreQuery = '';
   }
 
+  //create URL for fetch with values taken from selector elements
   var netflixQueryUrl = "https://unogsng.p.rapidapi.com/search?country_andorunique=US&audiosubtitle_andor=and&limit=25&subtitle=english&countrylist=78&audio=english&offset=0" + 
     seriesOrMovieQuery + 
     sortByQuery +
@@ -85,8 +87,8 @@ var createQuery = function() {
     endRatingQuery + 
     genreQuery;
 
+  //pass URL through to function that fetches from API
   netflixQuery(netflixQueryUrl);
-  console.log(netflixQuery)
 };
 
 var netflixQuery = function(queryUrl) {
@@ -102,6 +104,7 @@ var netflixQuery = function(queryUrl) {
     return response.json();
   })
   .then((data) => {
+    //save results to global array, render data to page after search
     currentQuery = data;
     renderToPage();
     console.log(data);
@@ -112,9 +115,12 @@ var netflixQuery = function(queryUrl) {
 };
 
 var renderToPage = function() {
-  var containerEl = document.querySelector("#card-container");
+  //clear any previous searches
   containerEl.innerHTML = '';
-  console.log(currentQuery);
+
+  var contentContainer = document.createElement("div");
+
+  //get length of result array, print each result
   for (var i = 0; i < currentQuery.results.length; i++) {
 
     var cellContainer = document.createElement("div");
@@ -122,15 +128,21 @@ var renderToPage = function() {
 
     var cardBody = document.createElement("div");
     cardBody.classList = 'card';
+    cardBody.setAttribute('data-equalizer-watch', '');
 
     var cardPoster = document.createElement("img");
     cardPoster.setAttribute('src', currentQuery.results[i].poster);
+    //if image doesn't load, use jquery from https://css-tricks.com/snippets/jquery/better-broken-image-handling/ to replace with placeholder
+    $(cardPoster).on("error", function() {
+      $(this).attr('src', './assets/img/300x420.png');
+    });
 
     var cardContent = document.createElement("div");
     cardContent.classList = 'card-section';
 
     var contentTitle = document.createElement("h4");
     contentTitle.textContent = currentQuery.results[i].title;
+    //look for instances of &#39; and replace with '
     contentTitle.textContent = contentTitle.textContent.replace('&#39;', "'");
 
     var contentSynopsis = document.createElement("p");
@@ -143,43 +155,179 @@ var renderToPage = function() {
     cardBody.appendChild(cardPoster);
     cardBody.appendChild(cardContent);
 
+    //add netflix ID and result array index as data attributes to button
+    var watchlistBtnEl = document.createElement("a");
+    watchlistBtnEl.setAttribute('data-nfid', currentQuery.results[i].nfid);
+    watchlistBtnEl.setAttribute('data-index', i);
+    watchlistBtnEl.classList = 'button expanded watch';
+
+    //function found at following link to check if value is in object in array without for loop https://stackoverflow.com/questions/8217419/how-to-determine-if-javascript-array-contains-an-object-with-an-attribute-that-e
+    if (savedToWatchlist.some(e => e.nfid === currentQuery.results[i].nfid)) {
+      watchlistBtnEl.textContent = 'Remove from Watchlist';
+    }
+    else {
+      watchlistBtnEl.textContent = 'Add to Watchlist';
+    }
+    
+    cardBody.appendChild(watchlistBtnEl);
+    cellContainer.appendChild(cardBody);
+
+    // previously included div in html to begin, caused issue with data equalizer attributes not working and equalizer breaking after add/remove from watchlist, switching views, etc
+    // to fix, needed to create the div dynamically, clear innerhtml of parent and render to page on every click or view switch
+    contentContainer.setAttribute('id', 'card-container');
+    contentContainer.setAttribute('data-equalizer','');
+    contentContainer.setAttribute('data-equalize-by-row', true);
+    contentContainer.classList = 'grid-x grid-margin-x small-up-1 medium-up-3';
+    
+    contentContainer.appendChild(cellContainer);
+    containerEl.appendChild(contentContainer);
+  }
+  //must initialize to allow foundation (equalize) to work
+  $(document).foundation();
+};
+
+var renderToWatchlist = function() {
+  var watchlistContainer = document.querySelector("#watchlist-container");
+  watchlistContainer.innerHTML = '';
+
+  var watchlistEl = document.createElement("div");
+
+  for (var i = 0; i < savedToWatchlist.length; i++) {
+
+    var cellContainer = document.createElement("div");
+    cellContainer.classList = 'cell';
+
+    var cardBody = document.createElement("div");
+    cardBody.classList = 'card';
+    cardBody.setAttribute('data-equalizer-watch', '');
+
+    var cardPoster = document.createElement("img");
+    cardPoster.setAttribute('src', savedToWatchlist[i].poster);
+    //if image doesn't load, use jquery from https://css-tricks.com/snippets/jquery/better-broken-image-handling/ to replace with placeholder
+    $(cardPoster).on("error", function() {
+      $(this).attr('src', './assets/img/300x420.png');
+    });
+
+    var cardContent = document.createElement("div");
+    cardContent.classList = 'card-section';
+
+    var contentTitle = document.createElement("h4");
+    contentTitle.textContent = savedToWatchlist[i].title;
+    contentTitle.textContent = contentTitle.textContent.replace('&#39;', "'");
+
+    var contentSynopsis = document.createElement("p");
+    contentSynopsis.textContent = savedToWatchlist[i].synopsis;
+    contentSynopsis.textContent = contentSynopsis.textContent.replace('&#39;', "'");
+
+    cardContent.appendChild(contentTitle);
+    cardContent.appendChild(contentSynopsis);
+
+    cardBody.appendChild(cardPoster);
+    cardBody.appendChild(cardContent);
+
     var watchlistBtnEl = document.createElement("a");
     watchlistBtnEl.classList = 'button expanded watch';
-    watchlistBtnEl.textContent = 'Add to Watchlist';
-    watchlistBtnEl.setAttribute('data-nfid', currentQuery.results[i].nfid);
+    watchlistBtnEl.textContent = 'Remove from Watchlist';
+    watchlistBtnEl.setAttribute('data-nfid', savedToWatchlist[i].nfid);
     watchlistBtnEl.setAttribute('data-index', i);
 
     cardBody.appendChild(watchlistBtnEl);
 
     cellContainer.appendChild(cardBody);
 
-    containerEl.appendChild(cellContainer);
+    // previously included div in html to begin, caused issue with data equalizer attributes not working and equalizer breaking after add/remove from watchlist, switching views, etc
+    // to fix, needed to create the div dynamically, clear innerhtml of parent and render to page on every click or view switch
+    watchlistEl.classList = 'grid-x grid-padding-x grid-margin-x small-up-2 large-up-5 medium-up-3 medium-cell-block-y';
+    watchlistEl.setAttribute('id', 'watchlist-content');
+    watchlistEl.setAttribute('data-equalizer','');
+    watchlistEl.setAttribute('data-equalize-by-row', true);
+    watchlistEl.appendChild(cellContainer);
+
+    watchlistContainer.appendChild(watchlistEl);
   }
+  //must initialize to allow foundation (equalize) to work
+  $(document).foundation();
 };
 
+//when the search view button is clicked, hide content in watchlist and make sure search content is visible
 var contentView = function() {
   searchViewBtn.classList = 'active-button primary button float-center';
   watchlistViewBtn.classList = 'nonactive-button clear button float-center';
   contentEl.classList = 'cell medium-auto medium-cell-block-container';
   watchlistEl.classList = 'cell medium-auto medium-cell-block-container hide';
+  renderToPage();
 };
 
+//when the watchlist button is clicked, hide content in search view and make sure watchlist is visible
 var watchlistView = function() {
   searchViewBtn.classList = 'nonactive-button clear button float-center';
   watchlistViewBtn.classList = 'active-button primary button float-center';
   contentEl.classList = 'cell medium-auto medium-cell-block-container hide';
   watchlistEl.classList = 'cell medium-auto medium-cell-block-container';
+  renderToWatchlist();
 };
 
 var contentClickHandler = function(event) {
   event.preventDefault();
 
+  //declare variable with target of button click
   var targetEl = event.target;
 
+  //if the button contains the watch class, declare variables for netflix ID and index in search array
   if (targetEl.matches(".watch")) {
     var contentId = targetEl.getAttribute("data-nfid");
     var contentIndex = targetEl.getAttribute("data-index");
-    console.log(contentId + ', ' + contentIndex);
+ 
+    var saveToStorage = true;
+    var removalIndex;
+
+    //declare var false if array already contains netflix ID for the title
+    for (var i = 0; i < savedToWatchlist.length; i++) {
+      if (savedToWatchlist[i].nfid === parseInt(contentId)) {
+        //if the title is already saved to the watchlist, declare saveToStorage var as false and set removal Index to location in saved array
+        saveToStorage = false;
+        removalIndex = i;
+        break;
+      }
+    }
+
+    //if save to storage var is true, push the title to the saved to watchlist array, save to localstorage
+    if (saveToStorage) {
+      savedToWatchlist.push(currentQuery.results[contentIndex]);
+      var resultForLocalstorage = JSON.stringify(savedToWatchlist);
+      localStorage.setItem('watchlist', resultForLocalstorage);
+
+      //cannot save to storage from watchlist since already in storage, only need to render to page
+      renderToPage();
+    }
+
+    //if save to storage var is false, find existing title in saved array with splice and value set at removal index, then save updated array to localstorage
+    if (!saveToStorage) {
+      savedToWatchlist.splice(removalIndex, 1);
+      var resultForLocalstorage = JSON.stringify(savedToWatchlist);
+      localStorage.setItem('watchlist', resultForLocalstorage);
+
+      //removing from watchlist could be done either from the watchlist or from the search view page, so run both functions
+      renderToPage();
+      renderToWatchlist();
+    }
+
+    //once item has either been saved or removed from watchlist, update text of button accordingly
+    if (targetEl.textContent === 'Add to Watchlist') {
+      targetEl.textContent = 'Remove from Watchlist';
+    }
+    else if (targetEl.textContent === 'Remove from Watchlist') {
+      targetEl.textContent = 'Add to Watchlist';
+    }
+  }
+};
+
+var loadSavedWatchlist = function() {
+  savedToWatchlist = JSON.parse(localStorage.getItem("watchlist"));
+  
+  //initialize empty array if nothing in localstorage
+  if (!savedToWatchlist) {
+    savedToWatchlist = [];
   }
 };
 
@@ -187,3 +335,5 @@ pageContentEl.addEventListener("click", contentClickHandler);
 searchButton.addEventListener("click", createQuery);
 searchViewBtn.addEventListener("click", contentView);
 watchlistViewBtn.addEventListener("click", watchlistView);
+
+loadSavedWatchlist();
